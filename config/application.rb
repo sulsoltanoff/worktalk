6955a85dbb5058e1600241aabd88849c6bfa6a9f
Sultan Soltanov <sultan@corpspace.org>
2023-05-13 12:12:41 +0500

    WT-1 Add app config

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,24 @@ require "pry"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-require_relative "../lib/bullet_train"
+## Load the specific APM agent
+# We rely on DOTENV to load the environment variables
+# We need these environment variables to load the specific APM agent
+Dotenv::Railtie.load
+require 'ddtrace' if ENV.fetch('DD_TRACE_AGENT_URL', false).present?
+require 'elastic-apm' if ENV.fetch('ELASTIC_APM_SECRET_TOKEN', false).present?
+require 'scout_apm' if ENV.fetch('SCOUT_KEY', false).present?
+
+if ENV.fetch('NEW_RELIC_LICENSE_KEY', false).present?
+  require 'newrelic-sidekiq-metrics'
+  require 'newrelic_rpm'
+end
+
+if ENV.fetch('SENTRY_DSN', false).present?
+  require 'sentry-ruby'
+  require 'sentry-rails'
+  require 'sentry-sidekiq'
+end
 
 module Worktalk
   class Application < Rails::Application
@@ -21,6 +38,10 @@ module Worktalk
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join('lib')
+
+    config.generators.javascripts = false
+    config.generators.stylesheets = false
 
     # See `config/locales/locales.yml` for a list of available locales.
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
@@ -30,5 +51,16 @@ module Worktalk
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     BulletTrain::Api.set_configuration(self)
+  end
+
+  def self.config
+    @config ||= Rails.configuration.x
+  end
+
+  def self.redis_ssl_verify_mode
+    # Introduced this method to fix the issue in heroku where redis connections fail for redis 6
+    # unless the redis verify mode is explicitly specified as none, we will fall back to the default 'verify peer'
+    # ref: https://www.rubydoc.info/stdlib/openssl/OpenSSL/SSL/SSLContext#DEFAULT_PARAMS-constant
+    ENV['REDIS_OPENSSL_VERIFY_MODE'] == 'none' ? OpenSSL::SSL::VERIFY_NONE : OpenSSL::SSL::VERIFY_PEER
   end
 end
