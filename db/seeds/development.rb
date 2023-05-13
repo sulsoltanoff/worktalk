94b13966d5466f811ca18ca20d1034beb6dd096a
Sultan Soltanov <sultan@corpspace.org>
2023-05-13 11:17:58 +0500

    WT-1 Add seeds for development env

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,1 +1,7 @@
 puts "🌱 Generating development environment seeds."
+
+# We use this stub to test `seeding?` for ActiveRecord models.
+User.create(email: "test@test.com", password: "956742469855eba772ea62b9f14d8626")
+user = User.find_by(email: "test@test.com")
+p "User is seeding: #{user.seeding?}"
+
